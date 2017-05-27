99bf56505ef3d95e2615d4843db3406aceca4b5a
xsniperxmail <xsniperxmail@gmail.com>
2017-05-27 19:46:09 -0400

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+// ConsoleApplication1.cpp : main project file.
+
+#include "stdafx.h"
+
+using namespace System;
+
+int main(array<System::String ^> ^args)
+{
+    Console::WriteLine(L"Hello World");
+    return 0;
+}
+
+int main(System RankException)
+{
+
+}
