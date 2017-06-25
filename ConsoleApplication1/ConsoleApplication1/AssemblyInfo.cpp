0bcbe4420ee3496071d16ae5f67b5e48eb052012
xsniperxmail <xsniperxmail@gmail.com>
2017-06-25 13:16:35 -0400

    Version Info Added

--- ConsoleApplication1/ConsoleApplication1/AssemblyInfo.cpp
+++ ConsoleApplication1/ConsoleApplication1/AssemblyInfo.cpp
@@ -26,12 +26,12 @@ using namespace System::Security::Permissions;
 //      Major Version 10
 //      Minor Version 0
 //      Build Number 15063
-//      Revision 297
+//      Revision 248
 //
 // You can specify all the value or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 
-[assembly:AssemblyVersionAttribute("1.0.*")];
+[assembly:AssemblyVersionAttribute("10.0.15063.248*")];
 
 [assembly:ComVisible(false)];
 
